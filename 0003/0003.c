d59dc12180fe1c3ab18871ce47167f406b711f75
RahulBarnwal09 <rbarnwal4056@gmail.com>
2021-01-14 09:07:04 +0530

    Update 0003.c

--- 0003/0003.c
+++ 0003/0003.c
@@ -3,7 +3,7 @@ int main()
 {
 int a,b,c;
 printf("Enter a number: ");
-scanf("%d",&a); //&a --> Address of a
+scanf("%d",&a); //&a --> Address of a,  %d--> Format specifier
 printf("Enter second number: ");
 scanf("%d",&b);
 c=a+b;
