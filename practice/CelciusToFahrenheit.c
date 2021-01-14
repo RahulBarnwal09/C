c0c9fa23cd1ff8cfb289d9edab4dbb0f3ad5c24a
RahulBarnwal09 <rbarnwal4056@gmail.com>
2021-01-14 09:45:19 +0530

    Update CelciusToFahrenheit.c

---
+++ practice/CelciusToFahrenheit.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float c;
+    printf("Enter the temperature: ");
+    scanf("%f",&c);
+    printf("Temp in Fahrenheit is: %f \n",1.8*c+32);
+    return 0;
+
+}
