9ee9a69d274b55c6e39d4827ca5fa40aa134ab4d
RahulBarnwal09 <rbarnwal4056@gmail.com>
2021-01-14 09:03:47 +0530

    Update 0003.c

---
+++ 0003/0003.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("Enter a number: ");
+scanf("%d",&a); //&a --> Address of a
+printf("Enter second number: ");
+scanf("%d",&b);
+c=a+b;
+printf("The sum is %d\n",c);
+}
