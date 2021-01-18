b369d05c060b7a913d23aed245193a5c94e18588
RahulBarnwal09 <rbarnwal4056@gmail.com>
2021-01-18 16:08:04 +0530

    Update practice

---
+++ practice/print10to20.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a=0;
+    while(a<=20)
+    {
+        if(a>=10)
+        {
+            printf("%d\n",a);
+        }
+        a++;
+    }
+    return 0;
+}
