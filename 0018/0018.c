4e068449aaf927d802a6f6407eb87fb19d8078eb
RahulBarnwal09 <rbarnwal4056@gmail.com>
2021-02-01 20:35:22 +0530

    Add 0018

--- 0018/0018.c
+++ 0018/0018.c
@@ -12,4 +12,7 @@ int main()
         printf("The marks of Student %d is %d\n",i+1,marks[i]);
     }
 //Each value in array generally takes 4 bytes(for integer data type), 1 byte (for char data type) etc. but it varies compiler to compiler.
+//Suppose the address of 1st element in array is 6204 then address for next element will be "Old Address + size of the data type".
+//eg:- if "1" and "2" are 2 elements of an array then if address of "1" is 6204 then address for "2" will be 6204+4=6208
+
 }
