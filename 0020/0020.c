df85856da4c044e1b6649effcbc82b5bb77a1e61
RahulBarnwal09 <rbarnwal4056@gmail.com>
2021-02-02 00:16:15 +0530

    Add 0020

---
+++ 0020/0020.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int marks[4];
+    int *ptr;
+    ptr=&marks[0];//we can also write "ptr=marks" instead of "ptr=&marks[0]"
+    for(int i=0;i<4;i++)
+    {
+        printf("Enter the value of marks for student %d: ",i+1);
+        scanf("%d",ptr);
+        ptr++;
+    }
+    for(int i=0;i<4;i++)
+    {
+        printf("The value of marks for student %d is: %d\n" ,i+1,marks[i]);
+    }
+
+    return 0;
+}
